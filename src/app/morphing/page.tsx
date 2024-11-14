c227039e70e4159b994c71fbdfedf87fe5b5a8b9
DiegoxK <diego.stv@outlook.es>
2024-11-14 09:56:17 -0500

    Create Morphing component with FigureAnimation for visual morphing effects in the morphing module

---
+++ src/app/morphing/page.tsx
@@ -0,0 +1,12 @@
+import FigureAnimation from "./animation";
+
+export default function Morphing() {
+  return (
+    <div>
+      Morphing
+      <div>
+        <FigureAnimation />
+      </div>
+    </div>
+  );
+}
